8a14cc6ecd6b5a4eca7f57ec4061c5b586345ace
GenieUU <youjin8019@naver.com>
2024-05-22 15:05:52 +0900

    중간 정리

--- src/Signup.js
+++ src/Signup.js
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.css';
 import Sidebar from './Sidebar';
 import logo from './_logo.png';
 import SideButton from './_SideButton.png';
 import EmailArrow from './_EmailArrow.png';
 
-const SignUp = ({ sideBarVisible, toggleSidebar, toggleSignUp }) => {
+const SignUp = ({ sideBarVisible, toggleSidebar }) => {
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('8~20자로 입력해주세요');
+  
+  // SUIdbox에 입력된 값이 변경 시 호출
+  const handleUserIdChange = (event) => {
+    setUserId(event.target.value);
+  };
+
+  // Password 입력란에 입력된 값이 변경 시 호출
+  const handleUserPasswordChange = (event) => {
+    setUserPassword(event.target.value);
+  };
+
+   // 초기 상태인지 확인하여 클래스를 동적으로 할당
+   const passwordInputClass = userPassword === '8~20자로 입력해주세요' ? 'SUPasswordbox InitialState' : 'SUPasswordbox';
+   
   return (
     <div className="SignUp">
       <div className="Background" />
       <img
-      className="Logo" 
-      src={logo} 
-      alt="Logo" 
+        className="Logo"
+        src={logo}
+        alt="Logo"
       />
       <img
-      className="SideButton"
-      src={SideButton}
-      alt="SideButton"
-      onClick={toggleSidebar}
+        className="SideButton"
+        src={SideButton}
+        alt="SideButton"
+        onClick={toggleSidebar}
       />
-      <Sidebar 
-      sideBarVisible={sideBarVisible}
-      toggleSidebar={sideBarVisible}
+      <Sidebar
+        sideBarVisible={sideBarVisible}
+        toggleSidebar={sideBarVisible}
       />
       <div className="AtSymbol">@</div>
-      <div className="SUIdbox" />
+      <input
+        className="SUIdbox"
+        type="text"
+        value={userId}
+        onChange={handleUserIdChange}
+      />
       <div className="SURepetitioncheckbox" />
       <div className="SUFinishbox" />
       <div className="SUNamebox" />
       <div className="SUEmailbox" />
       <div className="SUEmailselfbox" />
       <div className="SUEmailselectbox" />
-      <div className="SUPasswordbox" />
+      <input
+        className="SUPasswordbox"
+        type="text"
+        value={userPassword}
+        onChange={handleUserPasswordChange}
+      />
       <div className="SUPasswordcheckbox" />
       <div className="Idline"></div>
       <div className="Emailline"></div>
@@ -42,13 +68,12 @@ const SignUp = ({ sideBarVisible, toggleSidebar, toggleSignUp }) => {
       <div className="IdText">아이디</div>
       <div className="PasswordText">비밀번호</div>
       <div className="PasswordCheckText">비밀번호 확인</div>
-      <div className="PasswordHint">영문, 숫자, 특수문자 포함 8~20자리</div>
       <div className="PasswordCheckHint">비밀번호를 다시 입력해주세요.</div>
       <div className="EmailSelfHint">직접 입력</div>
       <img
-      className="EmailArrow" 
-      src={EmailArrow} 
-      alt="EmailArrow" 
+        className="EmailArrow"
+        src={EmailArrow}
+        alt="EmailArrow"
       />
       <div className="CheckDuplicationText">중복 확인</div>
       <div className="FinishText">가입완료</div>
